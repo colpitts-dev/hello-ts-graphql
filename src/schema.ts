4391be3d41d42dd5a20f6aa7ac7714607877a896
acolpitts <adam@colpitts.dev>
2023-01-30 12:14:38 -0400

    feat: add initial PersonType and queries

--- src/schema.ts
+++ src/schema.ts
@@ -1,4 +1,29 @@
-import { GraphQLObjectType, GraphQLSchema, GraphQLString } from "graphql";
+import {
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from "graphql";
+
+const peopleData = [
+  { id: "1", name: "Bill", email: "bill@example.com", age: 65 },
+  { id: "2", name: "Jane", email: "jane@example.com", age: 23 },
+  { id: "3", name: "Ted", email: "ted@example.com", age: 33 },
+  { id: "4", name: "John", email: "johndoe@example.com", age: 18 },
+];
+
+const PersonType = new GraphQLObjectType({
+  name: "Person",
+  description: "People are what make up the hyper[local] communities",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  }),
+});
 
 // Root query type
 const RootQuery = new GraphQLObjectType({
@@ -9,6 +34,25 @@ const RootQuery = new GraphQLObjectType({
       type: GraphQLString,
       resolve() {
         return "Hello TypeScript + GraphQL";
+      },
+    },
+    people: {
+      type: new GraphQLList(PersonType),
+      description: "Returns a list of all people on the platform",
+      resolve() {
+        return peopleData;
+      },
+    },
+    person: {
+      type: PersonType,
+      description: "Returns a single person by their ID",
+      args: {
+        id: { type: GraphQLID },
+      },
+      resolve(parent, args) {
+        return peopleData.find((person) => {
+          return person.id === args.id;
+        });
       },
     },
   },
